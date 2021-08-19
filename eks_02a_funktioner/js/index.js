9c49be42a7bc8c3287e8c181c81a955db0ae2c1e
MagnusThiemer <thiemer.jensen@gmail.com>
2021-08-19 10:34:42 +0200

    Add solution to assignment 02a

--- eks_02a_funktioner/js/index.js
+++ eks_02a_funktioner/js/index.js
@@ -1,6 +1,10 @@
 // "DOMContentLoaded" event'en sørger for, at Javascriptet først kører når hele HTML dokumentet er indlæst
 document.addEventListener("DOMContentLoaded", () => {
 
-	// skriv din kode her...
+hils();
+
+function hils(){
+	console.log('hej med dig!');
+}
 
 }); // Afslutter: DOMContentLoaded
