fea4aa54579993be8a4665b0be5df478fad1f94f
MagnusThiemer <thiemer.jensen@gmail.com>
2021-08-19 12:38:24 +0200

    Add solution to assignment 02c

--- eks_02c_funktioner/js/index.js
+++ eks_02c_funktioner/js/index.js
@@ -1,6 +1,15 @@
 // "DOMContentLoaded" event'en sørger for, at Javascriptet først kører når hele HTML dokumentet er indlæst
 document.addEventListener("DOMContentLoaded", () => {
 
-	// skriv din kode her...
+	function hils(navn){
+		return `Hej med dig, ${navn}`;
+	};
+	
+	let hilsenTilTroels = hils('Troels');
+	let hilsenTilMagnus = hils('Magnus');
+
+	console.log(hilsenTilTroels);
+	console.log(hilsenTilMagnus);
 
 }); // Afslutter: DOMContentLoaded
+
